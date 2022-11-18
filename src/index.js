c7760d4b204442eec81c88ac98e56b5da7e2bcfd
Domingos de Jesus Alcantara <domingos_alcantara@yahoo.com.br>
2022-11-18 11:19:02 -0300

    Refatorando o metodo buscarPeloTipo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const Favoritos = require("./models/favoritos");
 
 const arqFavoritos = "../data/Bookmarks";
 const isType = ["folder", "url"];
--- src/models/favoritos.js
+++ src/models/favoritos.js
@@ -1,5 +1,18 @@
 class Favoritos {
+  buscarPeloTipo(array){
+    return function (tipo){
+      return array.filter((item) => item.type === tipo);
+    };
+  }
 
+  quantItens(array, tipos){
+    const pastas = this.buscarPeloTipo;
+    const objItens = {
+      "Pastas": 0,        //Deve retornar a quantidade de pastas encontrada
+      "Links": 0          //Quantidade de links encontrados
+    }
+    return objItens;
+  }
 }
 
 module.exports = new Favoritos();
