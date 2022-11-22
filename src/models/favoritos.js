2bfd4599e71eafa09681645540ee79e0a3402bd8
Domingos de Jesus Alcantara <domingos_alcantara@yahoo.com.br>
2022-11-22 01:46:43 -0300

    Formatando

--- src/models/favoritos.js
+++ src/models/favoritos.js
@@ -1,31 +1,29 @@
 class Favoritos {
-  buscarPeloTipo(array){
-    return function (tipo){
+  buscarPeloTipo(array) {
+    return function (tipo) {
       return array.filter((item) => item.type === tipo);
     };
   }
-  
-  quantItens(array){
+
+  quantItens(array) {
     const listObjetos = this.buscarPeloTipo(array);
     //this.listObjetos(array);
     return function (tipos) {
       const objItens = {
-        "Pastas": listObjetos(tipos[0]).length,        //Deve retornar a quantidade de pastas encontrada
-        "Lista de Pastas": listObjetos(tipos[0]).map(item => item.name),
+        Pastas: listObjetos(tipos[0]).length, //Deve retornar a quantidade de pastas encontrada
+        Listagem: listObjetos(tipos[0]).map((item) => item.name),
         // "Links": listObjetos(tipos[1]).length,          //Quantidade de links encontrados
         // "Lista de Links": listObjetos(tipos[1])
-      }
+      };
       return objItens;
-    }
+    };
   }
 
-  retornandoApenasNome(array){
-    return array.map(item => item.name);
+  retornandoApenasNome(array) {
+    return array.map((item) => item.name);
   }
 
-  checarLinks(array){
-
-  }
+  checarLinks(array) {}
 }
 
 module.exports = new Favoritos();
