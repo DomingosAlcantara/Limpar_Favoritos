7c709f5d5283a70df16b4a67c2262c89bd77f6a2
Domingos de Jesus Alcantara <45907626+DomingosAlcantara@users.noreply.github.com>
2022-11-23 20:37:27 -0300

    Mudanças na função quantItens

--- src/models/favoritos.js
+++ src/models/favoritos.js
@@ -1,29 +1,34 @@
-class Favoritos {
-  buscarPeloTipo(array) {
+// class Favoritos {
+  const buscarPeloTipo = (array) => {
     return function (tipo) {
       return array.filter((item) => item.type === tipo);
     };
-  }
+  };
 
-  quantItens(array) {
-    const listObjetos = this.buscarPeloTipo(array);
-    //this.listObjetos(array);
+  const retornandoApenasNome = (array) => array.map((item) => item.name);
+
+  
+  const quantItens = (array) => {
+    const listObjetos = buscarPeloTipo(array);
     return function (tipos) {
       const objItens = {
         Pastas: listObjetos(tipos[0]).length, //Deve retornar a quantidade de pastas encontrada
-        Listagem: listObjetos(tipos[0]).map((item) => item.name),
+        // Listagem: listObjetos(tipos[0]).map((item) => item.name),
+        Listagem: retornandoApenasNome(listObjetos(tipos[0])),
         // "Links": listObjetos(tipos[1]).length,          //Quantidade de links encontrados
         // "Lista de Links": listObjetos(tipos[1])
       };
       return objItens;
     };
-  }
+  };
 
-  retornandoApenasNome(array) {
-    return array.map((item) => item.name);
-  }
 
-  checarLinks(array) {}
-}
+  // checarLinks(array) {}
+// }
 
-module.exports = new Favoritos();
+module.exports = {
+  buscarPeloTipo,
+  quantItens,
+  retornandoApenasNome,
+
+};
