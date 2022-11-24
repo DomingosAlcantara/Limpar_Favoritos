7b0263cb69220817b070a3f0dcdd811cf427e571
Domingos de Jesus Alcantara <45907626+DomingosAlcantara@users.noreply.github.com>
2022-11-23 21:35:24 -0300

    Redefinindo a função quantItens ao seu proposito Inicial

--- src/models/favoritos.js
+++ src/models/favoritos.js
@@ -14,8 +14,8 @@
       const objItens = {
         Pastas: listObjetos(tipos[0]).length, //Deve retornar a quantidade de pastas encontrada
         // Listagem: listObjetos(tipos[0]).map((item) => item.name),
-        Listagem: retornandoApenasNome(listObjetos(tipos[0])),
-        // "Links": listObjetos(tipos[1]).length,          //Quantidade de links encontrados
+        //Listagem: retornandoListaDeNomes(listObjetos(tipos[0])),
+        Links: listObjetos(tipos[1]).length,          //Quantidade de links encontrados
         // "Lista de Links": listObjetos(tipos[1])
       };
       return objItens;
@@ -23,12 +23,14 @@
   };
 
 
-  // checarLinks(array) {}
+  const checarLinks = (array) => {
+    
+  }
 // }
 
 module.exports = {
   buscarPeloTipo,
   quantItens,
-  retornandoApenasNome,
+  retornandoListaDeNomes,
 
 };
